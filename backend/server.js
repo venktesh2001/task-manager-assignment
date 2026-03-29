c7a010735ff888a72abd4c67461ca9b1aa2037c0
venktesh2001 <avenktesh2001@gmail.com>
2026-03-30 03:31:01 +0530

    prepare backend for Render

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const app = require("./app");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ DB Connected"));
-
 const PORT = process.env.PORT || 5000;
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ DB Connected"))
+  .catch(err => console.log(err));
+
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`🚀 Server running on ${PORT}`);
 });
