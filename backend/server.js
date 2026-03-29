0806826b2a7b6ae75af0ab983f0fbbccdde48579
venktesh2001 <avenktesh2001@gmail.com>
2026-03-30 02:34:32 +0530

    update frontend for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ DB Connected"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
